deb603614b764c6c2d3ac838985974f2ce742fa8
hugosenari <hugosenari@gmail.com>
2022-03-12 15:26:20 -0300

    chore: re index std_shims

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''APIs available in the latests version of Nim, backported to older stable releases'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."std_shims-master".url = "path:./master";
+  inputs."std_shims-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."std_shims-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
